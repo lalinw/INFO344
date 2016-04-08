11c1d4c6d9b9b25757a7194199e4cc48f29854b8
lalinw <lalinw@uw.edu>
2016-04-08 03:20:46 -0700

    'trying'

--- a1/index.php
+++ a1/index.php
@@ -1,1 +1,21 @@
-hello world
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php 
+    echo "hello world in php";
+    
+    $conn = new PDO('mysql:host=nba-db.c6uuvnaayrmz.us-west-2.rds.amazonaws.com:3306;dbname=nbadb', 'info344user', '344password'); 
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->query('SELECT * FROM player_stats')->fetchAll(PDO::FETCH_ASSOC);;
+
+    var_dump($stmt);
+?>
+    
+</body>
+</html>
+
