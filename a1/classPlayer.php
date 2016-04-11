9b384c1df1b2dffa12dcf086ed6a954c8da02232
lalinw <lalinw@uw.edu>
2016-04-11 11:37:09 -0700

    styling should be done, working on extra credits

--- a1/classPlayer.php
+++ a1/classPlayer.php
@@ -2,16 +2,20 @@
     class Player {
     //php, default is public
     
-        function Player($name, $team, $ppg, $m3pt, $reb, $ast, $stl, $blk, $turnover) {
+        function Player($name, $team, $ppg, $ast, $stl, $blk, $turnover, $gp, $min, $pt3, $fg, $ft, $reb) {
             $this->name = $name;
             $this->team = $team;
             $this->ppg = $ppg; 
-            $this->m3pt = $m3pt;
-            $this->reb = $reb;
             $this->ast = $ast;
             $this->stl = $stl;
             $this->blk = $blk;
             $this->turnover = $turnover;
+            $this->gp = $gp;
+            $this->min = $min;
+            $this->pt3 = $pt3;  //array attempt/made/percentages
+            $this->fg = $fg;    //array
+            $this->ft = $ft;    //array
+            $this->reb = $reb;  //array offense/defense/total
         }
         
         public function getName() {
@@ -30,10 +34,6 @@
             return $this->m3pt;     
         }
         
-        public function getreb() {
-            return $this->reb;     
-        }
-        
         public function getAst() {
             return $this->ast;     
         }
@@ -50,6 +50,30 @@
             return $this->turnover;     
         }
         
+        public function getGp() {
+            return $this->gp;     
+        }
+        
+        public function getMin() {
+            return $this->min;     
+        }
+        
+        public function getPt3() {
+            return $this->pt3;  //array 
+        }
+        
+        public function getFg() {
+            return $this->fg;    //array
+        }
+        
+        public function getFt() {
+            return $this->ft;    //array
+        }
+        
+        public function getReb() {
+            return $this->reb;  //array
+        }
+            
         public function getPlayer() {
             return $this->name." ".$this->team;
         }
--- a1/index.php
+++ a1/index.php
@@ -10,12 +10,12 @@
         <img src="NBA_logo.png" alt="NBA" height="150" width="">
         <form>
             <input type="text" name="search">
-            <input type="submit" value="Submit">
+            <input type="submit" value="search player">
         </form>
     </div>
     <div class="search" id="searchFeedback">
         <?php
-            if(isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
+            if(isset($_GET['search']) && $_GET['search'] != "") {
                 $playerSearch = $_REQUEST['search'];    //user form input 
                 echo 'You searched \'<b>'.$playerSearch.'\'</b>'; //some user feedback
             }
@@ -30,8 +30,11 @@
         //check connection
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        if(isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
-            //prepare SQL statement
+        if(isset($_GET['search']) && $_GET['search'] != "") {
+            //prepare SQL statement 
+            // $sql = "SELECT * 
+            //     FROM player_stats 
+            //     WHERE Name LIKE '%".$playerSearch."%'";
             $sql = "SELECT * 
                 FROM player_stats 
                 WHERE Name LIKE '%".$playerSearch."%'";
@@ -43,25 +46,31 @@
             } else {
                 $resultPlayerArray = array();   //array of Player objects
                 for($i = 0; $i < count($result); $i++) {
-                    //Player($name, $team, $ppg, $m3pt, $reb, $ast, $stl, $blk, $to)
+                    //Player($name, $team, $ppg, $ast, $stl, $blk, $to)
                     //creates new Player object and pushes to array
                     $plyr = new Player(
                         $result[$i]['Name'],
                         $result[$i]['Team'],
                         $result[$i]['PPG'],
-                        $result[$i]['M_3PT'], //3pt made
-                        $result[$i]['Rebounds_Tot'],
                         $result[$i]['Ast'],
                         $result[$i]['Stl'],
                         $result[$i]['Blk'],
-                        $result[$i]['TO']);
+                        $result[$i]['TO'],
+                        $result[$i]['GP'],
+                        $result[$i]['Min'],
+                        array($result[$i]['M_3PT'], $result[$i]['A_3PT'], $result[$i]['Pct_3PT']), //3pt made
+                        array($result[$i]['M_FG'], $result[$i]['A_FG'], $result[$i]['Pct_FG']),
+                        array($result[$i]['M_FT'], $result[$i]['A_FT'], $result[$i]['Pct_FT']),
+                        array($result[$i]['Rebounds_Off'], $result[$i]['Rebounds_Def'], $result[$i]['Rebounds_Tot']));
                     array_push($resultPlayerArray, $plyr);
+                    
                 }
                 for($i = 0; $i < count($resultPlayerArray); $i++) {       
                     //get player photo + info
                     $nm = $resultPlayerArray[$i]->getName();
                     $nm1 = str_replace(".", "", strtolower(explode(" ", $nm)[0]));
                     $nm2 = strtolower(explode(" ", $nm)[1]);
+                    //each namecard
                     echo "<div class='playerResult'>";
                         echo "<div class='profileImg'>";
                             echo "<img class='profilepic' itemprop='image' 
@@ -75,22 +84,52 @@
                         echo "<div class='playerTeam'>";
                             echo $resultPlayerArray[$i]->getTeam();
                         echo "</div>";
+                        
                         echo "<div class='playerInfo'>";
-                            // echo "PPG: ".$resultPlayerArray[$i]->getPpg()."/ Assist: ".$resultPlayerArray[$i]->getAst();
-                            echo "<div class='statName'>";
-                                echo "PPG:<br>Ast:<br>Stl:<br>Blk:<br>TO:";
+                            // column 1
+                            echo "<div class='col1'>";
+                                echo "<div class='statName'>";
+                                    echo "Ast:<br>Stl:<br>Blk:<br>TO:";
+                                echo "</div>";
+                                echo "<div class='stats'>";
+                                    echo $resultPlayerArray[$i]->getAst().
+                                    "<br>".
+                                    $resultPlayerArray[$i]->getStl().
+                                    "<br>".
+                                    $resultPlayerArray[$i]->getBlk().
+                                    "<br>".
+                                    $resultPlayerArray[$i]->getTurnover();
+                                echo "</div>";
                             echo "</div>";
-                            echo "<div class='stats'>";
-                                echo $resultPlayerArray[$i]->getPpg().
-                                "<br>".
-                                $resultPlayerArray[$i]->getAst().
-                                "<br>".
-                                $resultPlayerArray[$i]->getStl().
-                                "<br>".
-                                $resultPlayerArray[$i]->getBlk().
-                                "<br>".
-                                $resultPlayerArray[$i]->getTurnover();
-                            echo "</div>";              
+                            //column 2
+                            echo "<div class='col2'>";
+                                echo "<div class='statName'>";
+                                    echo "Rebounds:<br><br>3pt:<br>FG:<br>FT:";
+                                echo "</div>";
+                                echo "<div class='stats'>";
+                                    echo "Off ".$resultPlayerArray[$i]->getReb()[0]."/Def ".$resultPlayerArray[$i]->getReb()[1].
+                                        "<br>".
+                                        "Total ".$resultPlayerArray[$i]->getReb()[2]."<br>";
+                                    echo $resultPlayerArray[$i]->getPt3()[0]." (".$resultPlayerArray[$i]->getPt3()[2]."%)"."<br>";
+                                    echo $resultPlayerArray[$i]->getFg()[0]." (".$resultPlayerArray[$i]->getFg()[2]."%)"."<br>";
+                                    echo $resultPlayerArray[$i]->getFt()[0]." (".$resultPlayerArray[$i]->getFt()[2]."%)";
+                                echo "</div>";
+                                
+                            echo "</div>";  
+                            
+                            //column 3
+                            echo "<div class='col3'>";
+                                echo "PPG";
+                                echo "<div class='box ppg'>".$resultPlayerArray[$i]->getPpg()."</div>";
+                                echo "GP";
+                                echo "<div class='box gp'>".$resultPlayerArray[$i]->getGp()."</div>";
+                                echo "<div class='statName'>";
+                                    echo "Min:";
+                                echo "</div>";
+                                echo "<div class='stats'>";
+                                    echo $resultPlayerArray[$i]->getMin();
+                                echo "</div>";
+                            echo "</div>";  
                         echo "</div>";        
                         
                     echo "</div>";
