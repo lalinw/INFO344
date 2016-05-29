cb6a1b7ec1cb75310b1f0067eeade685831fd968
unknown <iGuest@is-m430m27-42.ischool.uw.edu>
2016-05-28 17:53:19 -0700

    make jsonp complete

--- a4/AzureCloudService1/WebRole1/nbaplayer_jsonp.php
+++ a4/AzureCloudService1/WebRole1/nbaplayer_jsonp.php
@@ -1,21 +1,16 @@
-http://stackoverflow.com/questions/1678214/javascript-how-do-i-create-jsonp
-
 <?php
-	$callback = $_GET['callback'];
+	// $callback = $_GET['search'];
     if(isset($_GET['search']) && $_GET['search'] != "") {
         $playerSearch = $_REQUEST['search'];
          //connecting to the DB in RDS
         $conn = new PDO('mysql:host=nba-db.c6uuvnaayrmz.us-west-2.rds.amazonaws.com:3306;dbname=nbadb', 'info344user', '344password'); 
         //check connection
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $sql = "SELECT TOP 1 * 
+        $sql = "SELECT * 
             FROM player_stats 
-            WHERE Name like " .$playerSearch;
+            WHERE Name LIKE "."'".$playerSearch."' LIMIT 1";
         $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);   //query the db
-
-        echo $callback . "(" . json_encode($result) . ")";
-
+        header('Content-Type: application/json;');
+        echo json_encode($result);
     }
 ?>
-
